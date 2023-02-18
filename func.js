e8dde025ae031a50fca7e95615f89d08b733d62c
NkosingiphileLT <luthotukuse6@gmail.com>
2023-02-18 13:12:42 +0200

    first commit

---
+++ func.js
@@ -0,0 +1,16 @@
+//Create a register function:
+//Create a user object 
+// Push this user object onto an array
+
+function register (user){
+   user.push(user);
+    }
+
+    register({
+        uname : 'Lutho',
+        email : 'lutho2gmaiil.conm',
+        password: 'VNL'
+    })
+
+    console.log(user)
+
